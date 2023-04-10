7d64f9b932f984b23a23427c46e48ff6daa861d5
sahelorang <sahel12345zelzeleh@gmail.com>
2023-04-11 00:42:55 +0430

    first commit

---
+++ src/components/Landing.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+
+//API
+import getCoin from "../services/api";
+
+//component
+import Loader from "./Loader";
+import Coin from "./Coin";
+
+//styles
+import styles from "./Landing.module.css";
+
+const Landing = () => {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const data = await getCoin();
+      console.log(data);
+      setCoins(data);
+    };
+    fetchAPI();
+  }, []);
+
+  const searchHandler = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchedCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <React.Fragment>
+      <input
+        placeholder="Search"
+        className={styles.input}
+        type="text"
+        value={search}
+        onChange={searchHandler}
+      />
+      {coins.length ? (
+        <div className={styles.coinContainer}>
+          {searchedCoins.map((coin) => (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              image={coin.image}
+              symbol={coin.symbol}
+              price={coin.current_price}
+              marketCap={coin.market_cap}
+              priceChange={coin.price_change_percentage_24h}
+            />
+          ))}
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Landing;
